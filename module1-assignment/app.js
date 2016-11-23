3ceaff8c69fe8a1b9d5814185067fb00317d1da4
akansharatra <a_arora@apple.com>
2016-11-23 14:55:40 -0800

    Corrected modified files

--- module1-assignment/app.js
+++ module1-assignment/app.js
@@ -1,36 +1,39 @@
 (function () {
   'use strict';
 
-  var shoppingList1 = [
-    {
-      name : "cookies",
-      quantity : "10"
-    },
-    {
-      name : "cookies",
-      quantity : "10"
-    },
-    {
-      name : "cookies",
-      quantity : "10"
-    },
-    {
-      name : "cookies",
-      quantity : "10"
-    },
-    {
-      name : "cookies",
-      quantity : "10"
+  angular.module("lunchCheck",[])
+    .controller("LunchCheckController", LunchCheckController);
+
+    LunchCheckController.$inject = ["$scope"];
+
+    function LunchCheckController($scope) {
+      $scope.lunchItems = "";
+       $scope.totalItems = "";
+       $scope.isValid="";
+      $scope.displayItems = function () {
+         var totalValue = calculateItems($scope.lunchItems);
+           $scope.totalItems = totalValue;
+ 
+       }
+ 
+       function calculateItems(content) {
+           var totalContentValue = "", output = "";
+ 
+           if(content == ""){
+             $scope.isValid = false;
+             output = "Please enter data first!!";
+           }else {
+             totalContentValue = content.split(",");
+             $scope.isValid=true;
+             if(totalContentValue.length > 3){
+               output = "Too Much!";
+ 
+             }else if (totalContentValue.length <=3){
+               output = "Enjoy!!";
+             }
+         }
+           return output;
+         }
+ 
     }
-
-  ]
-
-  angular.module("shoppingList",[])
-    .controller("shoppingListController", ShoppingListController);
-
-    ShoppingListController.$inject = ["$scope"];
-
-    function ShoppingListController($scope) {
-      $scope.buyShoppingList = shoppingList1;
-    }
-})();
+ })();
